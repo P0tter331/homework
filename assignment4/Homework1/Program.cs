a00ffdbc93cb55b39258891c1872d817a46f6331
P0tter331 <p0tterwang922@gmail.com>
2024-03-23 18:25:19 +0800

    第四周的软件构造作业提交

---
+++ assignment4/Homework1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Homework1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine("please input your number to the list(input 'exit' to quit)");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit") break;
+                try
+                {
+                    list.Add(int.Parse(input));
+                }
+                catch
+                {
+                    Console.WriteLine("invalid input!please input your number to the list(input 'exit' to quit)");
+                }
+            }
+            Console.WriteLine("input is over.");
+            list.ForEach(n => Console.WriteLine(n));
+            int max = list[0] ;
+            int min = max;
+            int sum = 0;
+
+            list.ForEach(n =>
+            {
+                if (max < n)
+                {
+                    max = n;
+                }
+            });
+            Console.WriteLine($"max={max}");
+
+            list.ForEach(n =>
+            {
+                if (min > n)
+                {
+                    min = n;
+                }
+            });
+            Console.WriteLine($"min={min}");
+
+            list.ForEach(n=>sum += n);
+            Console.WriteLine($"sum={sum}");
+
+        }
+    }
+}
---
+++ assignment4/Homework2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework2
+{
+    public class Clock
+    {
+        //定义事件委托
+        public delegate void TickHandler(object sender, EventArgs e);
+        public delegate void AlarmHandler(object sender, EventArgs e);
+
+        public event TickHandler Tick;
+        public event AlarmHandler Alarm;
+
+        // 触发Tick事件
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        // 触发Alarm事件
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                OnTick();
+                if (i%3==0)
+                {
+                    OnAlarm();
+                }
+            }
+        }
+    }
+    internal class Subcriber
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            //订阅事件
+            clock.Tick += (sender, e) => Console.WriteLine("Tick...Tick..");
+            clock.Alarm += (sender, e) => Console.WriteLine("Alarm! Ti's time to go to school!");
+
+            clock.Start();
+        }
+    }
+}
